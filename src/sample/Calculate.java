7acd5de4b9953d8dc84ea6fddf6945aca0c3148f
AAGalaktionov <a.galaktionov@yandex.ru>
2018-03-14 18:21:13 +0300

    it is work

--- src/sample/Calculate.java
+++ src/sample/Calculate.java
@@ -1,13 +1,51 @@
 package sample;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Calculate {
+    double x0 = 0.0;
+    double y0 = 2.0;
+/*    double tmpV0 = 10;
+    double tmpAngle = 45;*/
+    double h = 0.05;
     private InputText inputText;
-    private HashMap<Double, Double> list;
+    private ArrayList<Double> tmpVx = new ArrayList<>();
+    private ArrayList<Double> tmpVy = new ArrayList<>();
+    private ArrayList<Double> tmpX = new ArrayList<>();
+    private ArrayList<Double> tmpY = new ArrayList<>();
+
+
+    TreeMap<Double, Double> calculateTraectory(TreeMap<Double, Double> list, Double tmpV0, Double tmpAngle) {
+        list.clear();
+        tmpVx.clear();
+        tmpVy.clear();
+        tmpY.clear();
+        tmpX.clear();
+        tmpVx.add( tmpV0 * Math.cos(tmpAngle*Math.PI/180));
+
+        tmpVy.add(tmpV0 * Math.sin(tmpAngle*Math.PI/180));
+        tmpX.add(x0);
+        tmpY.add(y0);
+        list.put(tmpX.get(0), tmpY.get(0));
+        for (int i = 1; i < 100; i++) {
+            tmpVx.add(tmpVx.get(i-1));
+            tmpVy.add(tmpVy.get(i-1) - h * 9.8);
+            tmpX.add(tmpX.get(i-1) + h * tmpVx.get(i));
+            tmpY.add(tmpY.get(i-1) + h * tmpVy.get(i));
+            if(tmpY.get(i) > 0)
+            list.put((double) Math.round(tmpX.get(i)*1000)/1000, (double) Math.round(tmpY.get(i)*1000)/1000);
+        }
+
+        return list;
+    }
 
     public Calculate(InputText inputText) {
         this.inputText = inputText;
+    }
+    public Calculate() {
+
     }
 
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,18 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import jdk.nashorn.api.tree.Tree;
 
 import java.util.HashMap;
-import java.util.Random;
+import java.util.TreeMap;
 
 public class Controller {
 
@@ -24,40 +28,59 @@ public class Controller {
 
     @FXML
     public void initialize() {
+        TreeMap<Double, Double> line1 = new TreeMap<>();
+        TreeMap<Double, Double> line2 = new TreeMap<>();
+
+
+
+
+
         button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             try {
                 InputText inputText = new InputText(Double.valueOf(textField2.getText()), Double.valueOf(textField1.getText()));
                 System.out.println(inputText.getAngle() + " " + inputText.getSpeed());
+                Calculate calculate = new Calculate();
+                calculate.calculateTraectory(line1, inputText.getSpeed(), inputText.getAngle());
             } catch (NumberFormatException e) {
                 System.out.println("Некоректные входные данные!!!");
             }
-            //Очистка нужна, если нажмут на кнопку несколько раз, попробуй)))
+            //Очистка нужна, если нажмут на кнопку несколько раз
             lineChart.getData().clear();
+            lineChart.setHorizontalGridLinesVisible(true);
+
 
             //По нажатию рисуем линии, тестовый убогий код писать подругому, это для понимания
-            HashMap<Double, Double> line1 = new HashMap<>();
-            HashMap<Double, Double> line2 = new HashMap<>();
-            for (int i = 0; i < 11; i++) {
-                Random random = new Random();
-                double rand = random.nextDouble();
-                line1.put(i * 1.0, rand * i + i + 1);
-                line2.put(i * 1.0, rand * rand * i + 3);
-            }
 
+
+            double tmp = 4.00;
+            for (int i = 0; i < 5; i++) {
+                line2.put(tmp, 3.05);
+                tmp += 0.1;
+            }
+            line2.put(tmp-0.09, 4.05);
 
             XYChart.Series seriesForLine1 = new XYChart.Series();
             seriesForLine1.setName("Line one");
-            line1.entrySet().forEach(p -> seriesForLine1.getData().add(new XYChart.Data(p.getKey().toString(), p.getValue())));
+            for (HashMap.Entry<Double, Double> entry : line1.entrySet()) {
+                seriesForLine1.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+            }
             XYChart.Series seriesForLine2 = new XYChart.Series();
+            // seriesForLine2.nodeProperty();
             seriesForLine2.setName("Line two");
-            line2.entrySet().forEach(p -> seriesForLine2.getData().add(new XYChart.Data(p.getKey().toString(), p.getValue())));
+            for (HashMap.Entry<Double, Double> entry : line2.entrySet()) {
+                seriesForLine2.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+            }
 
             lineChart.getData().addAll(seriesForLine1, seriesForLine2);
 
 
         });
-        textField1.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> textField1.setText(""));
-        textField2.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> textField2.setText(""));
+        textField1.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            textField1.setText("");
+        });
+        textField2.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            textField2.setText("");
+        });
 
 
     }
