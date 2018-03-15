df06b2ec632b2033a9870bf96f0cee984acb8ee1
AAGalaktionov <a.galaktionov@yandex.ru>
2018-03-15 17:57:34 +0300

    it is work!!

--- src/sample/Calculate.java
+++ src/sample/Calculate.java
@@ -17,6 +17,14 @@ public class Calculate {
     private ArrayList<Double> tmpY = new ArrayList<>();
 
 
+    private ArrayList<Double> antmpX = new ArrayList<>();
+    private ArrayList<Double> antmpY = new ArrayList<>();
+
+    private ArrayList<Double> airtmpVx = new ArrayList<>();
+    private ArrayList<Double> airtmpVy = new ArrayList<>();
+    private ArrayList<Double> airtmpX = new ArrayList<>();
+    private ArrayList<Double> airtmpY = new ArrayList<>();
+
     TreeMap<Double, Double> calculateTraectory(TreeMap<Double, Double> list, Double tmpV0, Double tmpAngle) {
         list.clear();
         tmpVx.clear();
@@ -36,6 +44,53 @@ public class Calculate {
             tmpY.add(tmpY.get(i-1) + h * tmpVy.get(i));
             if(tmpY.get(i) > 0)
             list.put((double) Math.round(tmpX.get(i)*1000)/1000, (double) Math.round(tmpY.get(i)*1000)/1000);
+        }
+
+        return list;
+    }
+
+
+    TreeMap<Double, Double> calculateAnaliticalTraectory(TreeMap<Double, Double> list, Double tmpV0, Double tmpAngle) {
+        list.clear();
+
+        antmpY.clear();
+        antmpX.clear();
+        antmpX.add(x0);
+        antmpY.add(y0);
+        list.put(antmpX.get(0), antmpY.get(0));
+        double time = 0.0;
+        for (int i = 1; i < 100; i++) {
+            time+=0.05;
+            antmpX.add(antmpX.get(0) + tmpV0*Math.cos(tmpAngle*Math.PI/180)*time);
+            antmpY.add(antmpY.get(0) + tmpV0*Math.sin(tmpAngle*Math.PI/180)*time - 9.8*time*time/2);
+            if(antmpY.get(i) > 0)
+                /*list.put((double) Math.round(antmpX.get(i)*1000)/1000, (double) Math.round(antmpY.get(i)*1000)/1000);*/
+            list.put( antmpX.get(i), antmpY.get(i));
+        }
+
+        return list;
+    }
+
+
+    TreeMap<Double, Double> calculateAirResTraectory(TreeMap<Double, Double> list, Double tmpV0, Double tmpAngle) {
+        list.clear();
+        airtmpVx.clear();
+        airtmpVy.clear();
+        airtmpY.clear();
+        airtmpX.clear();
+        airtmpVx.add( tmpV0 * Math.cos(tmpAngle*Math.PI/180));
+
+        airtmpVy.add(tmpV0 * Math.sin(tmpAngle*Math.PI/180));
+        airtmpX.add(x0);
+        airtmpY.add(y0);
+        list.put(airtmpX.get(0), airtmpY.get(0));
+        for (int i = 1; i < 100; i++) {
+            airtmpVx.add(airtmpVx.get(i-1) - 6*Math.PI*0.0182*0.122*airtmpVx.get(i-1));
+            airtmpVy.add(airtmpVy.get(i-1) - h * 9.8 - 6*Math.PI*0.0182*0.122*airtmpVy.get(i-1));
+            airtmpX.add(airtmpX.get(i-1) + h * airtmpVx.get(i));
+            airtmpY.add(airtmpY.get(i-1) + h * airtmpVy.get(i));
+            if(airtmpY.get(i) > 0)
+                list.put((double) Math.round(airtmpX.get(i)*1000)/1000, (double) Math.round(airtmpY.get(i)*1000)/1000);
         }
 
         return list;
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import jdk.nashorn.api.tree.Tree;
@@ -25,11 +26,19 @@ public class Controller {
     private TextField textField2;
     @FXML
     private LineChart lineChart;
+    @FXML
+    private ToggleButton toggleAnalytically;
+    @FXML
+    private ToggleButton toggleNumerical;
+    @FXML
+    private ToggleButton toggleAirResistance;
 
     @FXML
     public void initialize() {
         TreeMap<Double, Double> line1 = new TreeMap<>();
         TreeMap<Double, Double> line2 = new TreeMap<>();
+        TreeMap<Double, Double> line3 = new TreeMap<>();
+        TreeMap<Double, Double> line4 = new TreeMap<>();
 
 
 
@@ -40,38 +49,72 @@ public class Controller {
                 InputText inputText = new InputText(Double.valueOf(textField2.getText()), Double.valueOf(textField1.getText()));
                 System.out.println(inputText.getAngle() + " " + inputText.getSpeed());
                 Calculate calculate = new Calculate();
+                Calculate calculate2 = new Calculate();
+
+                if(toggleNumerical.isSelected())
                 calculate.calculateTraectory(line1, inputText.getSpeed(), inputText.getAngle());
+
+                if(toggleAnalytically.isSelected())
+                    calculate2.calculateAnaliticalTraectory(line3, inputText.getSpeed(), inputText.getAngle());
+
+                if(toggleAirResistance.isSelected())
+                    calculate2.calculateAirResTraectory(line4, inputText.getSpeed(), inputText.getAngle());
+
+
+/*********************************************************************************************************/
+                /**TOGGLE**/
+/*********************************************************************************************************/
+                System.out.println("NUMERICAL!!!" +toggleNumerical.isSelected());
+                System.out.println("Analytically!!!" +toggleAnalytically.isSelected());
+                System.out.println("AirResistance!!!" +toggleAirResistance.isSelected());
+/*********************************************************************************************************/
+
+
             } catch (NumberFormatException e) {
                 System.out.println("Некоректные входные данные!!!");
             }
             //Очистка нужна, если нажмут на кнопку несколько раз
             lineChart.getData().clear();
-            lineChart.setHorizontalGridLinesVisible(true);
+          //  lineChart.setHorizontalGridLinesVisible(true);
 
 
             //По нажатию рисуем линии, тестовый убогий код писать подругому, это для понимания
 
 
-            double tmp = 4.00;
-            for (int i = 0; i < 5; i++) {
-                line2.put(tmp, 3.05);
-                tmp += 0.1;
-            }
-            line2.put(tmp-0.09, 4.05);
+
+            line2.put(4.0, 3.05);
+            line2.put(4.45, 3.05);
+            line2.put(4.60, 3.05);
+            line2.put(4.601, 3.95);
+            line2.put(4.602, 2.75);
 
             XYChart.Series seriesForLine1 = new XYChart.Series();
-            seriesForLine1.setName("Line one");
+            seriesForLine1.setName("Numerical");
             for (HashMap.Entry<Double, Double> entry : line1.entrySet()) {
                 seriesForLine1.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
             }
+
+            XYChart.Series seriesForLine3 = new XYChart.Series();
+            seriesForLine3.setName("Analitically");
+            for (HashMap.Entry<Double, Double> entry : line3.entrySet()) {
+                seriesForLine3.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+            }
+
+            XYChart.Series seriesForLine4 = new XYChart.Series();
+            seriesForLine4.setName("Air Resistance");
+            for (HashMap.Entry<Double, Double> entry : line4.entrySet()) {
+                seriesForLine4.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+            }
+
+
             XYChart.Series seriesForLine2 = new XYChart.Series();
             // seriesForLine2.nodeProperty();
-            seriesForLine2.setName("Line two");
+            seriesForLine2.setName("Basket");
             for (HashMap.Entry<Double, Double> entry : line2.entrySet()) {
                 seriesForLine2.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
             }
 
-            lineChart.getData().addAll(seriesForLine1, seriesForLine2);
+            lineChart.getData().addAll(seriesForLine1, seriesForLine3, seriesForLine4, seriesForLine2);
 
 
         });
